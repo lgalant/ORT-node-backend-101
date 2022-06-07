5a8e0986f4a04717143e60c51021e78b5bf23a16
Leandro Galanterni <leandro@Leandros-MacBook-Pro-2.local>
2022-06-07 14:04:18 -0300

    coment

--- queries.js
+++ queries.js
@@ -1,11 +1,14 @@
 const Pool = require('pg').Pool
-const databaseURL='postgres://tchvxezibrybmr:9424df5a9928fe9b9e69972bc436b4ba533283920b82e072228a2cd8002b6853@ec2-52-73-184-24.compute-1.amazonaws.com:5432/d4cb68hgmced7f'
-const pool = new Pool({
+const isProduction = process.env.NODE_ENV === "production";
+
 //  user: 'tchvxezibrybmr',
 //  host: 'ec2-52-73-184-24.compute-1.amazonaws.com',
 //  database: 'd4cb68hgmced7f',
 //  password: '9424df5a9928fe9b9e69972bc436b4ba533283920b82e072228a2cd8002b6853',
-  connectionString: databaseURL,
+const connectionString='postgres://tchvxezibrybmr:9424df5a9928fe9b9e69972bc436b4ba533283920b82e072228a2cd8002b6853@ec2-52-73-184-24.compute-1.amazonaws.com:5432/d4cb68hgmced7f'
+
+const pool = new Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
   port: 5432,
     ssl: {
         rejectUnauthorized: false,
